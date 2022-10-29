ca2bc05b0e95cf5176c761ebc24b9b3154491190
Lysak-Anna <hvlysak@gmail.com>
2022-10-29 15:09:00 +0300

    change task-7

--- js/task-07.js
+++ js/task-07.js
@@ -2,22 +2,11 @@ const controller = document.querySelector('#font-size-control');
 controller.addEventListener('input', inputControllerHandler);
 
 const text = document.querySelector('#text');
-text.style.fontSize = '16px';
-// let sizeOfFont = text.style.fontSize;
+
+
 function inputControllerHandler(event) {
     let currentValue = event.currentTarget.value;  
-    text.style.fontSize = currentValue;
-//   sizeOfFont = dataForFontSize;
-//    console.log(sizeOfFont); 
-     console.log( text.style.fontSize);
+    text.style.fontSize = `${currentValue}px`;
+
+    
 }
-console.log(text.style.fontSize);
-
-
-
-
-
-
-// Напиши скрипт, який реагує на зміну значення input#font - size - control(подія input)
-//  і змінює інлайн - стиль span#text, оновлюючи властивість font - size.
-//  В результаті, перетягуючи повзунок, буде змінюватися розмір тексту.
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const input = document.querySelector('input');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+const createButton = document.querySelector('[data-create]');
+
+
+const createBoxes = amount => {
+  const newElementsArray = [];
+  for (let i = 0; i < amount; i += 1) {
+    let newElement = document.createElement('div');
+    newElement.style.width = `${30 + 10 * i}px`;
+    newElement.style.height = `${30 + 10 * i}px`;
+    newElement.style.backgroundColor = getRandomHexColor();
+    newElementsArray.push(newElement);
+  
+  }
+
+  return newElementsArray;
+}
+
+createButton.addEventListener('click', () => {
+  let createdBoxes = createBoxes(input.value);
+  boxes.append(...createdBoxes);
+});
+
+destroyButton.addEventListener('click', () => boxes.innerHTML = '');
